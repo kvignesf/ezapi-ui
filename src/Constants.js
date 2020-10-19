23bdfd2743a82581e2001e222dc8c36fb2ff3404
Venkat <venkat@ezapi.ai>
2020-10-19 19:34:08 +0000

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,9 +1,9 @@
 export const Constants = {
   apiURL: "http://ezapi.ai",
-  localURL: "http://localhost:7744",
+  localURL: "http://instance-1.ezapi.ai:7744",
   USER_DETAILS: "LOGGEDIN-USER-DETAILS",
   linkedinApiUrl: "https://www.linkedin.com/oauth/v2",
   linkedClientId: "77hqgq6vt20utk",
   linkedClientSecret: "kUQEH6uLaNAiEVbb",
-  redirectUri: "http://localhost:3000/linkedin",
+  redirectUri: "http://instance-1.ezapi.ai:3000/linkedin",
 };
