0b6ea2781357b564ac33bbf6a3a9536a29c6c62c
Karthik-m <karthik7@m.com>
2021-04-29 11:39:15 +0530

    Implementing dashboard

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className='flex flex-col'>
+      <div className='fixed w-full top-0' style={{ height: '56px' }}>
+        appbar
+      </div>
+      <div className='h-full flex flex-row' style={{ marginTop: '56px' }}>
+        <div className='h-full w-52 fixed left-0'>left section</div>
+        <div className='ml-52 w-full min-h-screen'>Content</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
