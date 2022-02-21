40a4464ce25b56782ff407c39403430f0482a05e
manoj gurram <manoj.g@cumulations.com>
2022-02-21 09:51:37 +0530

    manoj

--- src/AddProject/addProjectQuery.js
+++ src/AddProject/addProjectQuery.js
@@ -190,9 +190,10 @@ const useUploadProjectDbs = (aiMutation, onSuccess) => {
     onSuccess: (data) => {
       if (data?.projectId) {
         if (
-          !_.isEmpty(projectDetails?.dbs) &&
+          (!_.isEmpty(projectDetails?.dbs) || (!_.isEmpty(projectDetails?.host) && !_.isEmpty(projectDetails?.port) && !_.isEmpty(projectDetails?.username) && !_.isEmpty(projectDetails?.password) && !_.isEmpty(projectDetails?.database) && !_.isEmpty(projectDetails?.type)))&&
           !_.isEmpty(projectDetails?.specs)
         ) {
+          console.log("$$$$$$","entered into data");
           aiMutation.mutate({
             projectId: data?.projectId,
           });
