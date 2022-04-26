04b9ca6a898940420638a7f3cb5190ace37823d3
Aakash Chid <aakash.c@cumulations.com>
2022-04-26 15:35:27 +0530

    Merge branch 'dev-aakash' into 'test'
    Dev aakash
    
    See merge request ezapi-designer/ezapi_designer_ui!73

--- src/ProjectPayment/CardDetailsForm.jsx
+++ src/ProjectPayment/CardDetailsForm.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import { ErrorMessage, Field, Form, Formik, useFormik } from 'formik';
+import React, { useState } from "react";
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { ErrorMessage, Field, Form, Formik, useFormik } from "formik";
 
-import cardDetailsSchema from './cardDetailsSchema';
-import { TextField } from '@material-ui/core';
-import classNames from 'classnames';
+import cardDetailsSchema from "./cardDetailsSchema";
+import { TextField } from "@material-ui/core";
+import classNames from "classnames";
 
 const CardDetailsForm = ({ disabled, formRef }) => {
   const stripe = useStripe();
@@ -18,15 +18,15 @@ const CardDetailsForm = ({ disabled, formRef }) => {
         },
       }
     );
-    console.log(token?.['id']);
+    console.log(token?.["id"]);
   }
   return (
     <div>
-      <p className="text-subtitle1 mb-3">Card Details</p>
+      <p className='text-subtitle1 mb-3'>Card Details</p>
 
       <Formik
         initialValues={{
-          cardHolderName: 'Test_CardHolder',
+          cardHolderName: "",
           card: false,
         }}
         validationSchema={cardDetailsSchema}
@@ -35,40 +35,40 @@ const CardDetailsForm = ({ disabled, formRef }) => {
         {({ errors, touched, setFieldValue }) => {
           return (
             <Form>
-              <div className="mb-4">
-                <p className="text-overline2 mb-2">Card Holder Name</p>
+              <div className='mb-4'>
+                <p className='text-overline2 mb-2'>Card Holder Name</p>
 
                 <Field
-                  id="cardHolderName"
-                  name="cardHolderName"
+                  id='cardHolderName'
+                  name='cardHolderName'
                   fullWidth
-                  color="primary"
-                  variant="outlined"
+                  color='primary'
+                  variant='outlined'
                   disabled={disabled}
                   error={
                     touched.cardHolderName && Boolean(errors.cardHolderName)
                   }
-                  helperText={<ErrorMessage name="cardHolderName" />}
+                  helperText={<ErrorMessage name='cardHolderName' />}
                   onKeyUp={(e) => {}}
                   inputProps={{
                     style: {
-                      height: '6px',
+                      height: "6px",
                     },
                   }}
                   as={TextField}
                 />
               </div>
 
-              <div className="mb-4">
+              <div className='mb-4'>
                 <CardElement
-                  className={classNames('border-1 rounded-md p-3', {
-                    'border-accent-red': touched?.card && Boolean(errors?.card),
-                    'border-neutral-gray5': !(
+                  className={classNames("border-1 rounded-md p-3", {
+                    "border-accent-red": touched?.card && Boolean(errors?.card),
+                    "border-neutral-gray5": !(
                       touched?.card && Boolean(errors?.card)
                     ),
                   })}
                   onChange={(e) => {
-                    setFieldValue('card', e?.complete);
+                    setFieldValue("card", e?.complete);
                   }}
                   options={{
                     hidePostalCode: true,
@@ -76,11 +76,11 @@ const CardDetailsForm = ({ disabled, formRef }) => {
                 />
                 {touched?.card && Boolean(errors?.card) && (
                   <p
-                    className="py-1"
+                    className='py-1'
                     style={{
-                      fontSize: '0.75rem',
-                      marginLeft: '1rem',
-                      color: '#f44336',
+                      fontSize: "0.75rem",
+                      marginLeft: "1rem",
+                      color: "#f44336",
                     }}
                   >
                     {errors?.card}
