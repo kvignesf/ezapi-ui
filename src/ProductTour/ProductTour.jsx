d8d5ec9ad495e0d8f187dfe681c7ae036ae26eef
manoj gurram <manoj.g@cumulations.com>
2022-05-21 12:03:15 +0530

    created productTour Component

---
+++ src/ProductTour/ProductTour.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import Dashboard from "../Dashboard";
+import { useGetProductVideos } from "./productTourQueries";
+import { PrimaryButton, TextButton } from "../shared/components/AppButton";
+
+const ProductTour = () => {
+  const url = `https://www.youtube.com/watch?v=DFsgbM8mRp8`;
+  const [index, setIndex] = useState(0);
+  const {
+    data: productVideosData,
+    isLoading: isFetchingOrders,
+    error: fetchOrdersError,
+    // isFetching: isFetchingOrdersBg,
+    // refetch: refetchOrders,
+  } = useGetProductVideos();
+
+  const handleNext = () => {
+    if (productVideosData?.productVideos) {
+      setIndex((index + 1) % productVideosData?.productVideos.length);
+    }
+  };
+  const handlePrev = () => {
+    if (productVideosData?.productVideos && index > 0) {
+      setIndex((index - 1) % productVideosData?.productVideos.length);
+    }
+  };
+  console.log("productVideos:", productVideosData);
+  return (
+    <Dashboard selectedIndex={4}>
+      <div className="mt-16">
+        <div className = "mb-4" align = "center">
+        <iframe
+          width="1000"
+          height="500"
+          src={
+            productVideosData &&
+            productVideosData.productVideos[index].youtubeURL
+          }
+          // src = {url}
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
+        <div style={{width:"1000px"}}>
+        <h6 className = "mt-4" style = {{textAlign:"start"}}>
+          {productVideosData && productVideosData.productVideos[index].title}
+        </h6>
+        <p className='mt-3 mb-2' style = {{textAlign:"start"}}>{productVideosData && productVideosData.productVideos[index].description}</p>
+        </div>
+        </div>
+        <div className="border-t-2 flex flex-row items-center justify-center pt-4">
+          <PrimaryButton classes="mr-3" onClick={handlePrev}>
+            Prev
+          </PrimaryButton>
+          <PrimaryButton onClick={handleNext}>Next</PrimaryButton>
+        </div>
+      </div>
+    </Dashboard>
+  );
+};
+export default ProductTour;
