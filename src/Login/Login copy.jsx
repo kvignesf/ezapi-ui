f363bca93b6c2858a62afb4882bf6a0af232b12a
Aakash Chid <aakash.c@cumulations.com>
2022-06-16 18:14:51 +0530

    loginPageSSOEmail Changes

---
+++ src/Login/Login copy.jsx
@@ -0,0 +1,300 @@
+import React, { useState, useRef, useEffect } from "react";
+import Card from "@material-ui/core/Card";
+import AppIcon from "../shared/components/AppIcon";
+import { Field, ErrorMessage, Form, Formik } from "formik";
+import CloseIcon from "@material-ui/icons/Close";
+import * as Yup from "yup";
+import { Select, MenuItem, OutlinedInput, Grid } from "@material-ui/core";
+import { LinkedIn } from "react-linkedin-login-oauth2";
+import linkedin from "react-linkedin-login-oauth2/assets/linkedin.png";
+import sso from "../icons/ssoLogo3.svg";
+import { useHistory, useLocation } from "react-router-dom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { getAccessToken } from "../shared/storage";
+import { FormHelperText } from "@mui/material";
+import { TextField } from "@material-ui/core";
+import Logo from "../static/images/logo/connectoLogoWithName.svg";
+import Constants from "../shared/constants";
+import { Dialog } from "@material-ui/core/index";
+import { PrimaryButton } from "../shared/components/AppButton";
+import routes from "../shared/routes";
+import Colors from "../shared/colors";
+import { useLogin } from "../shared/query/authQueries";
+import {
+  clearSession,
+  setAccessToken,
+  setFirstName,
+  setLastName,
+  setUserId,
+  setEmailId,
+} from "../shared/storage";
+import client, { endpoint } from "../shared/network/client";
+import { CircularProgress } from "@material-ui/core";
+import _ from "lodash";
+import { isUserLoggedIn } from "../shared/utils";
+import { useQuery } from "react-query";
+
+const acc_token = getAccessToken();
+const Login = () => {
+  const formRef = useRef();
+  const [ssoLoggedIn, setSsoLoggedIn] = useState(false);
+  const [dialog, setDialog] = useState(false);
+  const [formValues, setFormValues] = useState();
+  const [ssoError, setSsoError] = useState();
+  const history = useHistory();
+  const redirect_uri = `${window.location.origin}/linkedin`;
+  // console.log(formValues);
+  const {
+    error: loginError,
+    isLoading: isLoggingIn,
+    isSuccess: isLoginSuccess,
+    mutate: login,
+    reset: resetLogin,
+  } = useLogin();
+
+  const handleSuccess = (data) => {
+    if (data?.code && !_.isEmpty(data?.code)) {
+      login({ linkedInAuthToken: data?.code, redirect_uri: redirect_uri });
+    }
+  };
+
+  useEffect(() => {
+    if (isUserLoggedIn()) {
+      history.push({
+        pathname: routes.projects,
+        state: { allow: false },
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isUserLoggedIn()) {
+      history.push({
+        pathname: routes.projects,
+        state: { allow: false },
+      });
+    }
+  }, [ssoLoggedIn]);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    const code = queryParams.get("code");
+    if (code) {
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ code: code }),
+      };
+      fetch(
+        process.env.REACT_APP_API_URL + "/auth_workos",
+        // "http://localhost:7744/auth_workos",
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          if ("jwtToken" in data && "userData" in data) {
+            setAccessToken(data?.jwtToken);
+            setFirstName(data?.userData?.firstName);
+            setLastName(data?.userData?.lastName);
+            setUserId(data?.userData?.user_id);
+            setEmailId(data?.userData?.email);
+            setSsoLoggedIn(true);
+          } else {
+          }
+        });
+    }
+  }, []);
+
+  function SSOLogin(values) {
+    // console.log(orgID);
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        orgName: values.org,
+        domain: values.dom,
+      }),
+    };
+    fetch(
+      process.env.REACT_APP_API_URL + "/sso_url",
+      // "http://localhost:7744/sso_url",
+      requestOptions
+    )
+      .then((response) => response.json())
+
+      .then((data) => {
+        // console.log(data);
+        if (!data?.url && data?.error) {
+          // console.log("inside error");
+          setSsoError(data?.error);
+        }
+        if (data?.url) {
+          setSsoError();
+          window.location = data?.url;
+        }
+      });
+  }
+
+  const handleFailure = (error) => {
+    resetLogin();
+    setAccessToken(null);
+  };
+
+  if (isLoginSuccess && !isLoggingIn && !loginError) {
+    history.replace({
+      pathname: routes.projects,
+      state: { allow: false },
+    });
+    return null;
+  }
+  const handleCloseDialog = () => {
+    setDialog(false);
+  };
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <Card className='w-1/2 max-w-sm flex flex-col justify-center items-center p-5'>
+        <img
+          src={Logo}
+          alt='conektto logo'
+          className='mb-4 p-3'
+          style={{ maxWidth: "128px" }}
+        />
+        {!dialog && (
+          <>
+            {!isLoggingIn && (
+              <div className='mb-2 flex flex-col '>
+                <LinkedIn
+                  className='mb-2'
+                  clientId={Constants.linkedClientId}
+                  onFailure={handleFailure}
+                  onSuccess={handleSuccess}
+                  redirectUri={encodeURIComponent(redirect_uri)}
+                  redirectPath={"/signin"}
+                  scope='r_liteprofile r_emailaddress'
+                >
+                  <img
+                    src={linkedin}
+                    alt='Log in with Linked In'
+                    style={{ maxWidth: "180px" }}
+                    className='w-full'
+                  />
+                </LinkedIn>
+
+                <img
+                  src={sso}
+                  onClick={() => {
+                    setDialog(true);
+                  }}
+                  alt='Log in with SSO'
+                  style={{ maxWidth: "180px" }}
+                  className='w-full cursor-pointer ... '
+                />
+              </div>
+            )}
+
+            {isLoggingIn && (
+              <div>
+                <CircularProgress size={20} />
+              </div>
+            )}
+
+            {loginError && (
+              <p className='text-overline2 text-accent-red'>
+                {loginError?.message}
+              </p>
+            )}
+          </>
+        )}
+
+        {dialog && (
+          <>
+            <div className='mb-2 flex flex-col'>
+              {" "}
+              {/* <Formik
+                initialValues={{
+                  org: formValues?.org ?? "",
+                  dom: formValues?.dom ?? "",
+                }}
+                innerRef={formRef}
+                validationSchema={Yup.object().shape({
+                  email: Yup.string().required("EmailID is required."),
+                })}
+                enableReinitialize
+              >
+                {({ errors, touched, values }) => {
+                  setFormValues(values);
+                  return (
+                    <Form>
+                      <Grid item xs={10}>
+                        <p className='text-mediumLabel mt-2 py-2'>Email ID</p>
+                        <Field
+                          id='email'
+                          name='email'
+                          fullWidth
+                          color='primary'
+                          error={touched.host && Boolean(errors.host)}
+                          helperText={
+                            <ErrorMessage name='dom'>
+                              {(msg) => (
+                                <div style={{ color: "red" }}>{msg}</div>
+                              )}
+                            </ErrorMessage>
+                          }
+                          onKeyUp={(e) => {
+                            const { value } = e.target;
+                          }}
+                          style={{
+                            height: "100px",
+
+                            width: "100%",
+                            color: "primary",
+                            backgroundColor: "#ffffff",
+                            borderColor: touched.type && errors.type && "red",
+                          }}
+                          variant='outlined'
+                          as={TextField}
+                        />
+                      </Grid>
+                    </Form>
+                  );
+                }}
+              </Formik> */}
+            </div>
+
+            {ssoError && (
+              <p className=' mt-5 text-overline2 text-accent-red ml-5 my-2'>
+                {ssoError}
+              </p>
+            )}
+
+            <div className='border-t-2 mt-4 border-neutral-gray7 flex flex-row items-end justify-end pt-4'>
+              <PrimaryButton
+                onClick={() => {
+                  setDialog(false);
+                }}
+              >
+                Back
+              </PrimaryButton>
+              <PrimaryButton
+                onClick={() => {
+                  if (formRef.current) {
+                    formRef.current.handleSubmit();
+                    if (formRef.current.isValid) {
+                      console.log(formValues);
+                      SSOLogin(formValues);
+                    }
+                  }
+                  // console.log("clicked next");
+                }}
+              >
+                Next
+              </PrimaryButton>
+            </div>
+          </>
+        )}
+      </Card>
+    </div>
+  );
+};
+
+export default Login;
