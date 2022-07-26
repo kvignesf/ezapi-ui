1671f7136fa2b10434eb7e1cbe973da8c85293f8
Aakash Chid <aakash.c@cumulations.com>
2022-07-26 16:38:18 +0530

    foreign Key Columns bug

--- src/Project/OperationDetails/PathParams/PathParams.jsx
+++ src/Project/OperationDetails/PathParams/PathParams.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { useRecoilValue, useRecoilState, useGetRecoilValueInfo_UNSTABLE } from "recoil";
+import {
+  useRecoilValue,
+  useRecoilState,
+  useGetRecoilValueInfo_UNSTABLE,
+} from "recoil";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -31,7 +35,6 @@ import tableAtom from "../../../shared/atom/tableAtom";
 import primaryAtom from "../../../shared/atom/primaryAtom";
 import tablesDataAtom from "../../../shared/atom/tablesDataAtom";
 
-
 const PathParams = ({ request = true }) => {
   const [operationDetails, setOperationDetails] = useRecoilState(
     operationAtomWithMiddleware
@@ -43,30 +46,31 @@ const PathParams = ({ request = true }) => {
   const { fetch: fetchParentName } = useGetParentName();
   const { fetch: fetchFullPath } = useGetFullPath();
 
-  let final_arr = []
   const itemDropped = (item) => {
-    if(isColumn(item)){
-      if(item?.foreign)
-      {
-        final_arr.push(item?.foreign?.table);        
-        let condition = tablesData.filter((table)=>{
-          return table.name === item.foreign.table
-        })[0].selectedColumns.filter((column)=>{
-          return column.name === item.foreign.column
-        })[0]
-        
-        while(condition?.foreign){       
-        final_arr.push(condition.foreign.table);
-        condition = tablesData.filter((table)=>{
-          return table.name === condition.foreign.table
-        })[0].selectedColumns.filter((column)=>{
-          return column.name === condition.foreign.column
-        })[0]
-       
-      }
-      }
-      else{
-        final_arr = []
+    let final_arr = [];
+    if (isColumn(item)) {
+      if (item?.foreign) {
+        final_arr.push(item?.foreign?.table);
+        let condition = tablesData
+          .filter((table) => {
+            return table.name === item.foreign.table;
+          })[0]
+          .selectedColumns.filter((column) => {
+            return column.name === item.foreign.column;
+          })[0];
+
+        while (condition?.foreign) {
+          final_arr.push(condition.foreign.table);
+          condition = tablesData
+            .filter((table) => {
+              return table.name === condition.foreign.table;
+            })[0]
+            .selectedColumns.filter((column) => {
+              return column.name === condition.foreign.column;
+            })[0];
+        }
+      } else {
+        final_arr = [];
       }
       setPrimaryKeyRef(final_arr);
     }
