a827c74d305188440983389179d961e3ba683142
LAPTOP-PBIADF1K\krish <kashyap@ezapi.ai>
2022-09-05 15:03:07 +0530

    log change - new conektto logos

--- src/shared/components/EzapiLogo.jsx
+++ src/shared/components/EzapiLogo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { ReactComponent as Logo } from "../../static/images/logo/connectoLogo.svg";
+import { ReactComponent as Logo } from "../../static/images/logo/newconnectoLogo.svg";
 
 const EzapiLogo = () => {
   return <Logo alt='conektto logo' className='bg-white w-8 h-8 rounded-sm' />;
